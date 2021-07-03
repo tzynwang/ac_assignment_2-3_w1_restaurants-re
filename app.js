2148c34f3c3c88d8b8e9aa1fb4bbaef929a16b40
tzyn (Windows Fork) <tzyn.wang@gmail.com>
2021-07-03 13:15:57 +0800

    fix: update entrypoint

--- app.js
+++ app.js
@@ -80,5 +80,5 @@ app.use(function (req, res) {
 })
 
 app.listen(port, () => {
-  console.log(`Express is listening on localhost:${port}`)
+  console.log(`Express is listening on localhost:${port}/welcome`)
 })
